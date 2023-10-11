45aa0e725c77d12458e5ddee01ec588abf8dbcfb
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-10-11 12:50:58 -0300

    feat: Criado validações no formulario de cadastro

--- src/screens/createMeal/index.tsx
+++ src/screens/createMeal/index.tsx
@@ -1,9 +1,8 @@
-
 import {ArrowLeft} from 'phosphor-react'
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { formattedDate } from '../../util/formatDate';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useState } from 'react';
 interface IMeal {
   name: string,
   description: string,
@@ -15,19 +14,35 @@ interface IMeal {
 
 export function CreateMeal() {
   const [isDiet, setIsDiet] = useState<boolean | null>(null)
+  const {register, handleSubmit, reset, setError, clearErrors, formState: {errors}} = useForm<IMeal>()
 
-  const {register, handleSubmit, reset} = useForm<IMeal>()
+  function checkStateIsDiet(){
+    if(isDiet === null){
+      setError('isDiet', {
+        message: "Selecione sim ou não"
+      })
+    }
+  }
 
   const handleRegisterNewMeal: SubmitHandler<IMeal> = (dados) =>{
     const { data, hour } = dados;
-    if(data && hour){
-      const form = {
-        ...dados,
-        createdAt: formattedDate(data, hour),
-        isDiet: isDiet
+
+    if(Object.keys(errors).length === 0){
+      if(data && hour && isDiet !== null){
+        const form = {
+          ...dados,
+          createdAt: formattedDate(data, hour),
+          isDiet: isDiet
+        }
+        console.log(form);
+        reset()
+        setIsDiet(null)
       }
-      console.log(form);
-      reset()
+      if(isDiet === null){
+        setError('isDiet', {
+          message: "Selecione sim ou não"
+        })
+      }
     }
   }
 
@@ -43,30 +58,52 @@ export function CreateMeal() {
         <div className='flex flex-col gap-1 w-full'>
           <label className=' text-titleXS text-BaseGray200 font-nunito' htmlFor="">Nome</label>
           <input
-            className=' w-full p-3.5 border border-BaseGray500 rounded-md text-bodyM text-BaseGray100 font-nunito' type="text"
-            {...register("name")}
+            className={`w-full p-3.5 border border-BaseGray500 rounded-md text-bodyM text-BaseGray100 font-nunito ${errors.name ? ' bg-BrandRedLight border-BrandRedDark' : ''}`}
+            type="text"
+            placeholder={errors.name ? errors.name.message : "Refeição"}
+            {...register("name",
+              {
+                required: "Nome deve ser maior que 2 letras",
+                minLength: 2
+              }
+            )}
           />
+
         </div>
         <div className='flex flex-col gap-1 w-full'>
           <label className=' text-titleXS text-BaseGray200 font-nunito' htmlFor="">Descrição</label>
-          <input
-            className=' w-full p-3.5 h-28 border border-BaseGray500 rounded-md text-bodyM text-BaseGray100 font-nunito' type="text "
-            {...register("description")}
+          <textarea
+            className= {`w-full p-3.5 h-28 border border-BaseGray500 rounded-md text-bodyM text-BaseGray100 font-nunito ${errors.description ? ' bg-BrandRedLight border-BrandRedDark' : ''}`}
+            placeholder={errors.description ? errors.description.message : "Descreva sua refeição"}
+            {...register("description",
+              {
+                required: 'Preencha uma descrição',
+                minLength: 1
+              }
+            )}
           />
         </div>
         <div className='flex gap-6'>
           <div className='flex flex-col gap-1 w-full'>
             <label className=' text-titleXS text-BaseGray200 font-nunito' htmlFor="">Data</label>
             <input
-              className=' w-full p-3.5 border border-BaseGray500 rounded-md text-bodyM text-BaseGray100 font-nunito' type="date"
-              {...register("data")}
+              className={`w-full p-3.5 border border-BaseGray500 rounded-md text-bodyM text-BaseGray100 font-nunito ${errors.data ? ' bg-BrandRedLight border-BrandRedDark' : ''}`}
+              type="date"
+              placeholder={errors.data && errors.data.message}
+              {...register("data",
+                {required: "Data invalida"}
+              )}
             />
           </div>
           <div className='flex flex-col gap-1 w-full'>
             <label className=' text-titleXS text-BaseGray200 font-nunito' htmlFor="">Hora</label>
             <input
-              className=' w-full p-3.5 border border-BaseGray500 rounded-md text-bodyM text-BaseGray100 font-nunito' type="time"
-              {...register("hour")}
+              className={`w-full p-3.5 border border-BaseGray500 rounded-md text-bodyM text-BaseGray100 font-nunito ${errors.hour ? ' bg-BrandRedLight border-BrandRedDark' : ''}`}
+              type="time"
+              placeholder={errors.hour && errors.hour.message}
+              {...register("hour",
+                {required: "Hora invalida"}
+              )}
             />
           </div>
         </div>
@@ -74,18 +111,31 @@ export function CreateMeal() {
           <label htmlFor="">Esta dentro da dieta?</label>
           <div className='flex gap-6 w-full'>
             <button
-              type='button' onClick={() => setIsDiet(true)}
-              className={`mt-auto w-full button-select-green ${isDiet === true ? 'active' : ''}`}>
-                <div className=' w-2 h-2 rounded-full bg-BrandGreenDark'></div>Sim
+              type='button'
+              onClick={() => {
+                  setIsDiet(true),
+                  clearErrors('isDiet')
+                }
+              }
+              className={`mt-auto w-full button-select-green ${isDiet === true ? 'active' : ''} ${errors.isDiet ? 'animate-pulse bg-BrandGreenLight' : ''}`}
+                >
+                  <div className=' w-2 h-2 rounded-full bg-BrandGreenDark'></div>Sim
             </button>
             <button
-              type='button' onClick={() => setIsDiet(false)}
-              className={`mt-auto w-full button-select-red ${isDiet === false ? 'active' : ''}`}>
-                <div className=' w-2 h-2 rounded-full bg-BrandRedDark'></div>Não
+              type='button'
+              onClick={() => {
+                setIsDiet(false),
+                clearErrors('isDiet')
+              }}
+              className={`mt-auto w-full button-select-red ${isDiet === false ? 'active' : ''} ${errors.isDiet ? 'animate-pulse bg-BrandRedLight' : ''}`}
+                >
+                  <div className=' w-2 h-2 rounded-full bg-BrandRedDark disabled:bg-red-500'></div>Não
             </button>
           </div>
+          {errors.isDiet && <p>{errors.isDiet.message}</p>}
         </div>
         <button
+          onClick={() => checkStateIsDiet()}
           type='submit'
           className='active-solid-button mt-auto w-full'
             >Cadastrar refeição

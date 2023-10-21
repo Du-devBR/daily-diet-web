f3dd88282faa4b6f4143a6a7bd63f2a990356811
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-10-20 21:49:37 -0300

    feat: criado função getTokenUser para decodificar token

---
+++ src/util/tokenUtils.ts
@@ -0,0 +1,12 @@
+import jwtDecode from "jwt-decode"
+
+interface IToken {
+  sub: string
+  iat: number
+}
+
+export const getTokenUser = (token: string) => {
+  const {sub} = jwtDecode<IToken>(token)
+
+  return sub
+}

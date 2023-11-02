0465e158a30092792b2d198a371ff406e9ce467c
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-11-02 13:24:21 -0300

    feat: criado action para create new password

--- src/redux/actions/regenerateNewPassword/resetPassword-action.ts
+++ src/redux/actions/regenerateNewPassword/resetPassword-action.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { API_DAILY_DIET } from "../../../service/Api";
+import { Ipassword } from "../../../screens/regenerateNewPassword/resetPassword";
 
 export const fetchTokenForResetPassword = createAsyncThunk('reset/fetchTokenForResetPassword', async (token: string) => {
   try {
@@ -11,6 +12,15 @@ export const fetchTokenForResetPassword = createAsyncThunk('reset/fetchTokenForR
     throw error
   }
 
+})
+
+export const createNewPassword = createAsyncThunk('reset/createNewPassword', async (password: Ipassword) => {
+  const localStorageToken = localStorage.getItem('reset')
+  const option = {
+    Authorization: `Bearer ${localStorageToken}`
+  }
+  const response = await axios.put(`${API_DAILY_DIET}/redefinepassword?token=${localStorageToken}`, password, {headers: option})
+  return response.status
 })
 
 

d6a4c5189be61587ebf039a345892c44b7fe0669
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-11-02 13:25:43 -0300

    feat: concluido interação para recuperar senha

--- src/screens/regenerateNewPassword/resetPassword/index.tsx
+++ src/screens/regenerateNewPassword/resetPassword/index.tsx
@@ -3,13 +3,17 @@ import logo from "../../../assets/logo.png";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchTokenForResetPassword } from "../../../redux/actions/regenerateNewPassword/resetPassword-action";
+import {
+  createNewPassword,
+  fetchTokenForResetPassword,
+} from "../../../redux/actions/regenerateNewPassword/resetPassword-action";
 import { AppDispatch } from "../../../redux/store";
 import { selectLoading } from "../../../redux/reducer/regenerateNewPassword/resetPassword-reducer";
 import Swal from "sweetalert2";
 import { useForm } from "react-hook-form";
+import { TailSpin } from "react-loader-spinner";
 
-interface Ipassword {
+export interface Ipassword {
   password: string;
   confirmePassword: string;
 }
@@ -34,10 +38,13 @@ export function ResetPassword() {
       const searchParams = new URLSearchParams(location.search);
       const token = searchParams.get("token");
       if (token) {
+        localStorage.removeItem("token");
         dispatch(fetchTokenForResetPassword(token)).then((result) => {
           if (result.payload) {
+            localStorage.setItem("reset", token);
             console.log("ok");
           } else {
+            localStorage.removeItem("reset");
             Swal.fire({
               icon: "error",
               title: "Token expirado, por favor ir até pagina de login!",
@@ -57,16 +64,28 @@ export function ResetPassword() {
   }, [dispatch, location.search, navigate]);
 
   const handleSubmitNewPassword = async (data: Ipassword) => {
-    console.log(data);
+    if (Object.keys(errors).length === 0) {
+      try {
+        await dispatch(createNewPassword(data)).then((result) => {
+          if (result.payload) {
+            localStorage.removeItem("reset");
+            Swal.fire({
+              icon: "success",
+              title: "Senha atualizada com sucesso!",
+              showConfirmButton: true,
+              confirmButtonText: "Login",
+            }).then((resul) => {
+              if (resul.isConfirmed) {
+                navigate("/login");
+              }
+            });
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
-
-  if (loading) {
-    return <div>loading</div>;
-  }
-
-  if (loading) {
-    return <div>loading</div>;
-  }
 
   return (
     <div className=" bg-BrandGreenLight min-h-screen flex flex-col">
@@ -77,106 +96,121 @@ export function ResetPassword() {
           </Link>
           <img className=" self-center" src={logo} alt="" />
         </header>
-        <form
-          action=""
-          onSubmit={handleSubmit(handleSubmitNewPassword)}
-          className=" w-full rounded-3xl bg-BaseGray700 p-10 calc-main-height "
-        >
-          <h1 className=" text-titleM text-BaseGray100 font-nunito text-center mb-8 ">
-            Redefinir senha
-          </h1>
-          <p className=" text-BaseGray200 text-bodyM font-nunito mb-8">
-            Abaixo, você irá informar sua nova senha. Lembrece de criar uma
-            senha forte com
-            <strong>
-              (Letras maiúsculas e minúsculas, numeros e caracteres especiais)
-            </strong>
-            e não a compartilhe com mais ninguem!
-          </p>
-          <div className="flex flex-col gap-2 w-full">
-            <label
-              htmlFor=""
-              className=" text-titleXS font-nunito text-BaseGray200"
-            >
-              Nova senha
-            </label>
-            <input
-              className="w-full mb-4 px-2 py-4 rounded-lg bg-BaseGray500 text-bodyM text-BaseGray200 font-nunito outline-none placeholder:text-BaseGray300"
-              type="password"
-              placeholder="********"
-              {...register("password", {
-                required: "Senha obrigatorio",
-                minLength: {
-                  value: 8,
-                  message: "A senha deve ser maior que 8 caracteres.",
-                },
-                pattern: {
-                  value:
-                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                  message:
-                    "A senha deve conter pelo menos 8 caracteres, 1 maiúscula, 1 minúscula, 1 número e 1 caractere especial",
-                },
-              })}
-              onFocus={() => setToggleAviso(true)}
-              onBlur={() => setToggleAviso(false)}
+        {loading ? (
+          <div className=" flex justify-center items-center">
+            <TailSpin
+              height="80"
+              width="80"
+              color="#4fa94d"
+              ariaLabel="tail-spin-loading"
+              radius="1"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
             />
-            <div
-              className={` ${
-                toggleAviso ? "flex" : "hidden"
-              } flex-col w-full bg-BrandRedLight rounded-lg p-2 mb-4 gap-1`}
-            >
-              <strong className=" text-titleXS text-BaseGray200 font-nunito">
-                Sua senha precisa ter:{" "}
+          </div>
+        ) : (
+          <form
+            action=""
+            onSubmit={handleSubmit(handleSubmitNewPassword)}
+            className=" w-full rounded-3xl bg-BaseGray700 p-10 calc-main-height "
+          >
+            <h1 className=" text-titleM text-BaseGray100 font-nunito text-center mb-8 ">
+              Redefinir senha
+            </h1>
+            <p className=" text-BaseGray200 text-bodyM font-nunito mb-8">
+              Abaixo, você irá informar sua nova senha. Lembrece de criar uma
+              senha forte com
+              <strong>
+                (Letras maiúsculas e minúsculas, numeros e caracteres especiais)
               </strong>
-              <span className=" text-bodyS text-BaseGray300 font-nunito">
-                Minimo de 8 caracteres
-              </span>
-              <span className=" text-bodyS text-BaseGray300 font-nunito">
-                Minimo de 1 caractere especial '*/_@'
-              </span>
-              <span className=" text-bodyS text-BaseGray300 font-nunito">
-                Minimo de 1 numemo
-              </span>
-              <span className=" text-bodyS text-BaseGray300 font-nunito">
-                Letras maiusculas e minusculas
-              </span>
+              e não a compartilhe com mais ninguem!
+            </p>
+            <div className="flex flex-col gap-2 w-full">
+              <label
+                htmlFor=""
+                className=" text-titleXS font-nunito text-BaseGray200"
+              >
+                Nova senha
+              </label>
+              <input
+                className="w-full mb-4 px-2 py-4 rounded-lg bg-BaseGray500 text-bodyM text-BaseGray200 font-nunito outline-none placeholder:text-BaseGray300"
+                type="password"
+                placeholder="********"
+                {...register("password", {
+                  required: "Senha obrigatorio",
+                  minLength: {
+                    value: 8,
+                    message: "A senha deve ser maior que 8 caracteres.",
+                  },
+                  pattern: {
+                    value:
+                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                    message:
+                      "A senha deve conter pelo menos 8 caracteres, 1 maiúscula, 1 minúscula, 1 número e 1 caractere especial",
+                  },
+                })}
+                onFocus={() => setToggleAviso(true)}
+                onBlur={() => setToggleAviso(false)}
+              />
+              <div
+                className={` ${
+                  toggleAviso ? "flex" : "hidden"
+                } flex-col w-full bg-BrandRedLight rounded-lg p-2 mb-4 gap-1`}
+              >
+                <strong className=" text-titleXS text-BaseGray200 font-nunito">
+                  Sua senha precisa ter:{" "}
+                </strong>
+                <span className=" text-bodyS text-BaseGray300 font-nunito">
+                  Minimo de 8 caracteres
+                </span>
+                <span className=" text-bodyS text-BaseGray300 font-nunito">
+                  Minimo de 1 caractere especial '*/_@'
+                </span>
+                <span className=" text-bodyS text-BaseGray300 font-nunito">
+                  Minimo de 1 numemo
+                </span>
+                <span className=" text-bodyS text-BaseGray300 font-nunito">
+                  Letras maiusculas e minusculas
+                </span>
+              </div>
             </div>
-          </div>
-          <div className="flex flex-col gap-2 w-full">
-            <label
-              htmlFor=""
-              className=" text-titleXS font-nunito text-BaseGray200"
-            >
-              Confirme a nova senha
-            </label>
-            <input
-              className="w-full mb-4 px-2 py-4 rounded-lg bg-BaseGray500 text-bodyM text-BaseGray200 font-nunito outline-none placeholder:text-BaseGray300"
-              type="password"
-              placeholder="********"
-              {...register("confirmePassword", {
-                required: "Confirme sua senha obrigatorio!",
-                minLength: {
-                  value: 8,
-                  message: "Senha pdeve ser igual a digitada",
-                },
-                validate: (value) =>
-                  value === getValues("password") ||
-                  "Senha diferente da digitada",
-              })}
-            />
-          </div>
-          {
-            <div className="flex flex-col self-start mb-4">
-              <span className=" text-bodyS text-BrandRedDark font-nunito">
-                {errors.password && errors.password.message}
-              </span>
-              <span className=" text-bodyS text-BrandRedDark font-nunito">
-                {errors.confirmePassword && errors.confirmePassword.message}
-              </span>
+            <div className="flex flex-col gap-2 w-full">
+              <label
+                htmlFor=""
+                className=" text-titleXS font-nunito text-BaseGray200"
+              >
+                Confirme a nova senha
+              </label>
+              <input
+                className="w-full mb-4 px-2 py-4 rounded-lg bg-BaseGray500 text-bodyM text-BaseGray200 font-nunito outline-none placeholder:text-BaseGray300"
+                type="password"
+                placeholder="********"
+                {...register("confirmePassword", {
+                  required: "Confirme sua senha obrigatorio!",
+                  minLength: {
+                    value: 8,
+                    message: "Senha pdeve ser igual a digitada",
+                  },
+                  validate: (value) =>
+                    value === getValues("password") ||
+                    "Senha diferente da digitada",
+                })}
+              />
             </div>
-          }
-          <button className=" active-solid-button w-full mt-8">Enviar</button>
-        </form>
+            {
+              <div className="flex flex-col self-start mb-4">
+                <span className=" text-bodyS text-BrandRedDark font-nunito">
+                  {errors.password && errors.password.message}
+                </span>
+                <span className=" text-bodyS text-BrandRedDark font-nunito">
+                  {errors.confirmePassword && errors.confirmePassword.message}
+                </span>
+              </div>
+            }
+            <button className=" active-solid-button w-full mt-8">Enviar</button>
+          </form>
+        )}
       </div>
     </div>
   );

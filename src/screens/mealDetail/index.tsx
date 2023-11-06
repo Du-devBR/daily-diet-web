b4042ac9683d83a048557bf96cd45cfa7d3e33ad
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-11-06 14:48:00 -0300

    feat: adicionado loading detailMeal

--- src/screens/mealDetail/index.tsx
+++ src/screens/mealDetail/index.tsx
@@ -11,10 +11,10 @@ import {
 import {
   selectLoading,
   selectMeals,
-  selectStatus,
 } from "../../redux/reducer/meals/meals-reducer";
 import { formattedDataAndHour } from "../../util/formatDate";
 import Swal from "sweetalert2";
+import { Loading } from "../../components/loading";
 
 export function MealDetail() {
   const { id } = useParams();
@@ -22,7 +22,6 @@ export function MealDetail() {
   const dispatch = useDispatch<AppDispatch>();
   const meals = useSelector(selectMeals);
   const loading = useSelector(selectLoading);
-  const statusCode = useSelector(selectStatus);
 
   const [openModal, setOpenModal] = useState(false);
 
@@ -42,7 +41,7 @@ export function MealDetail() {
           if (status === 200) {
             Swal.fire({
               icon: "success",
-              title: "Atualizado com sucesso!",
+              title: "Refeição execluida",
               timer: 2000,
               showConfirmButton: false,
             }).then((result) => {
@@ -53,7 +52,7 @@ export function MealDetail() {
           } else {
             Swal.fire({
               icon: "error",
-              title: "Erro ao atualizar",
+              title: "Erro ao excluir",
               timer: 2000,
               showConfirmButton: false,
             }).then((result) => {
@@ -69,12 +68,6 @@ export function MealDetail() {
     }
   };
 
-  if (loading) {
-    return <div>Loading....</div>;
-  }
-
-  console.log(statusCode);
-
   return (
     <div className=" bg-BaseGray500 w-full min-h-screen flex flex-col justify-center items-center md:max-w-5xl m-auto">
       <header className="flex items-start w-full text-center p-6">
@@ -86,34 +79,40 @@ export function MealDetail() {
         </h1>
       </header>
       <div className="flex flex-col gap-6 w-full calc-manege-height px-6 py-10 bg-BaseGray700 rounded-t-3xl">
-        <main className=" flex flex-col gap-6">
-          <div className="flex flex-col gap-2">
-            <h2 className=" text-BaseGray100 text-titleM font-nunito">
-              {meals[0]?.name}
-            </h2>
-            <p className=" text-BaseGray200 text-bodyM font-nunito">
-              {meals[0]?.description}
-            </p>
+        {loading ? (
+          <div className="m-auto">
+            <Loading />
           </div>
-          <div className="flex flex-col gap-2">
-            <h2 className=" text-BaseGray100 text-titleS font-nunito">
-              Data e hora
-            </h2>
-            <span className=" text-BaseGray200 text-bodyM font-nunito">
-              {formattedDataAndHour(meals[0]?.createdAt)}
-            </span>
-          </div>
-          <div className="flex gap-2 items-center py-2 px-4 rounded-full bg-BaseGray600 mr-auto">
-            <div
-              className={`w-2 h-2 rounded-full ${
-                meals[0]?.isDiet ? "bg-BrandGreenDark" : " bg-BrandRedDark"
-              } `}
-            ></div>
-            <span className="text-BaseGray100 text-bodyS font-nunito">
-              {meals[0]?.isDiet ? "dentro da dieta" : "fora da dieta"}
-            </span>
-          </div>
-        </main>
+        ) : (
+          <main className=" flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <h2 className=" text-BaseGray100 text-titleM font-nunito">
+                {meals[0]?.name}
+              </h2>
+              <p className=" text-BaseGray200 text-bodyM font-nunito">
+                {meals[0]?.description}
+              </p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h2 className=" text-BaseGray100 text-titleS font-nunito">
+                Data e hora
+              </h2>
+              <span className=" text-BaseGray200 text-bodyM font-nunito">
+                {formattedDataAndHour(meals[0]?.createdAt)}
+              </span>
+            </div>
+            <div className="flex gap-2 items-center py-2 px-4 rounded-full bg-BaseGray600 mr-auto">
+              <div
+                className={`w-2 h-2 rounded-full ${
+                  meals[0]?.isDiet ? "bg-BrandGreenDark" : " bg-BrandRedDark"
+                } `}
+              ></div>
+              <span className="text-BaseGray100 text-bodyS font-nunito">
+                {meals[0]?.isDiet ? "dentro da dieta" : "fora da dieta"}
+              </span>
+            </div>
+          </main>
+        )}
         <footer className=" flex  flex-col gap-2 mt-auto">
           <Link to={`/edit/${id}`} className=" active-solid-button w-full">
             <PencilSimpleLine className=" w-4 h-4" />
